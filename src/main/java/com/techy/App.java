11dfc7965faa47a789cc45e500f2b2757ff51dd1
ngasso <ngassowilliam@gmail.com>
2022-07-22 15:10:53 -0400

    webapptechy

---
+++ src/main/java/com/techy/App.java
@@ -0,0 +1,13 @@
+package com.techy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
